cda25459ed1252607914c181e4f5ce4c09ba7e37
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2025-01-26 17:48:45 -0800

    Level Select

---
+++ scripts/menu.gd
@@ -0,0 +1,37 @@
+extends Control
+
+@onready var MainMenu = $MainMenu
+@onready var MainButtons = $MainMenu/MainButtons
+@onready var StartButton = $MainMenu/MainButtons/StartButton
+@onready var LevelsButton = $MainMenu/MainButtons/LevelsButton
+@onready var LevelSelect = $MainMenu/LevelSelect
+var game = preload("res://scenes/game.tscn")
+
+func _on_start_button_pressed() -> void:
+	start_level(0)
+
+func _on_levels_button_pressed() -> void:
+	MainButtons.visible = false
+	LevelSelect.visible = true
+
+func _on_button_pressed() -> void:
+	MainButtons.visible = true
+	LevelSelect.visible = false
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel") and !MainMenu.visible:
+		GAME.INSTANCE.queue_free()
+		MainMenu.visible = true
+		MainButtons.visible = true
+		LevelSelect.visible = false
+
+func start_level(level) -> void:
+	add_child(game.instantiate())
+	GAME.INSTANCE.start_level(level)
+	MainMenu.visible = false
+
+func _on_level_1_pressed() -> void:
+	start_level(0)
+
+func _on_level_2_pressed() -> void:
+	start_level(1)
