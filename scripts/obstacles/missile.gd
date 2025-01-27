9536d1597ab634d3455283f8ff8537d984c4df3f
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2025-01-27 00:03:55 -0800

    Correct missile collision

--- scripts/obstacles/missile.gd
+++ scripts/obstacles/missile.gd
@@ -8,8 +8,8 @@ func _on_body_entered(body: Node) -> void:
 	if body is BUBBLE:
 		GAME.INSTANCE.despawn(body)
 		SOUNDS.INSTANCE.pop.play()
-	elif (body is TileMapLayer) or (body is CollisionObject2D and body.get_collision_layer_value(2)):
-		GAME.INSTANCE.despawn(self)
 	elif body is PLAYER:
 		GAME.INSTANCE.reset()
 		SOUNDS.INSTANCE.pop.play()
+	else:
+		GAME.INSTANCE.despawn(self)
